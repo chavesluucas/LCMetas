8b123120d5954d6cd653f05158f49ed78af3aedc
Chaves <luucas.chaves@gmail.com>
2022-09-09 09:21:09 -0300

    Configuração do cors

---
+++ backend/src/main/java/tech/lucaschaves/lcmeta/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+//Esse é o Cors, o arquivo responsável por permitir a gente hospedar o front em uma plataforma e o backend em outra
+//Por natureza não tem como fazer a ligação de uma plataforma com a outra, e esses códigos abaixo permite isso
+//Ele foi copiado direto  do github do eveto
+//https://github.com/devsuperior/sds-dsmeta/tree/main/episodio2
+package tech.lucaschaves.lcmeta.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.headers().frameOptions().disable();
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
